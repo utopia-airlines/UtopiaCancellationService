6240387e961e9a5ece3426ff7b9dd6a5739f9965
henryjyc <kinghenryjyc@gmail.com>
2019-06-07 15:53:42 -0400

    Add SpringInitializr files.

---
+++ src/main/java/com/st/utopia/cancellationservice/CancellationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.st.utopia.cancellationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancellationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancellationServiceApplication.class, args);
+	}
+
+}
